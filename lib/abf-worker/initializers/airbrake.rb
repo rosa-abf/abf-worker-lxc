c1e8e0def1031f4b10d07f3ff6e61d842d086bbc
Vokhmin Alexey V <avokhmin@gmail.com>
2013-11-21 21:55:15 +0400

    Use errbit instead of rollbar, Break rpm build if less than 3 packages

--- lib/abf-worker/initializers/airbrake.rb
+++ lib/abf-worker/initializers/airbrake.rb
@@ -2,6 +2,7 @@ require 'airbrake'
 
 Airbrake.configure do |config|
   config.api_key  = APP_CONFIG['airbrake_api_key']
-  config.host     = 'api.rollbar.com'
-  config.secure   = true
+  config.host     = 'errbit.rosalinux.ru'
+  config.port     = 80
+  config.secure   = config.port == 443
 end
