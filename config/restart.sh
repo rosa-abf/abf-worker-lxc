abfac38998ae26a0a6953fdf58249256437bc91b
Vokhmin Alexey V <avokhmin@gmail.com>
2014-04-17 22:28:56 +0400

    #376: added script for restarting

---
+++ config/restart.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+echo '--> abf-worker-lxc: restart.sh'
+
+source ~/.bash_profile
+
+
+if [ `ps aux | grep god | grep -v grep` == '' ] ; then
+  cd ~/abf-worker/current
+  rvm ruby-2.1.0@abf-worker exec bundle exec rake abf_worker:clean_up ENV=production
+  ENV=production VAGRANT_DEFAULT_PROVIDER=lxc INTERVAL=5 COUNT=1 QUEUE=rpm_worker_default,rpm_worker GROUP=rpm RESQUE_TERM_TIMEOUT=600 TERM_CHILD=1 CURRENT_PATH='/home/rosa/abf-worker/current' BACKGROUND=yes rvm ruby-2.1.0@abf-worker exec bundle exec god -c /home/rosa/abf-worker/current/config/resque.god
+else
+  echo '--> worker is not dead'
+  exit 1
+fi
+
+exit 0
