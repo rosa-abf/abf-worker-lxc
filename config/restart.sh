65c6558cb427d3bfd718efca1943bbba61367982
Vokhmin Alexey V <avokhmin@gmail.com>
2014-04-18 00:40:42 +0400

    Updated restart.sh

--- config/restart.sh
+++ config/restart.sh
@@ -7,6 +7,7 @@ source ~/.bash_profile
 
 if [ `ps aux | grep god | grep -v grep` == '' ] ; then
   cd ~/abf-worker/current
+  rm -f rm -f ~/abf-worker/shared/pids/*
   rvm ruby-2.1.0@abf-worker exec bundle exec rake abf_worker:clean_up ENV=production
   ENV=production VAGRANT_DEFAULT_PROVIDER=lxc INTERVAL=5 COUNT=1 QUEUE=rpm_worker_default,rpm_worker GROUP=rpm RESQUE_TERM_TIMEOUT=600 TERM_CHILD=1 CURRENT_PATH='/home/rosa/abf-worker/current' BACKGROUND=yes rvm ruby-2.1.0@abf-worker exec bundle exec god -c /home/rosa/abf-worker/current/config/resque.god
 else
