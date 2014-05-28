4d694c17ed1f106beef2e4c9b351177c165b986b
Vokhmin Alexey V <avokhmin@gmail.com>
2014-05-29 02:36:17 +0400

    Updated version of ruby

--- config/restart.sh
+++ config/restart.sh
@@ -8,7 +8,7 @@ if [[ `ps aux | grep god | grep -v gre |  wc -l` == "0" ]] ; then
   cd ~/abf-worker/current
   rm -f ~/abf-worker/shared/pids/*
   rvm ruby-2.1.1@abf-worker exec bundle exec rake abf_worker:clean_up ENV=production
-  ENV=production VAGRANT_DEFAULT_PROVIDER=lxc INTERVAL=5 COUNT=1 QUEUE=rpm_worker_default,rpm_worker GROUP=rpm RESQUE_TERM_TIMEOUT=600 TERM_CHILD=1 CURRENT_PATH='/home/rosa/abf-worker/current' BACKGROUND=yes rvm ruby-2.1.0@abf-worker exec bundle exec god -c /home/rosa/abf-worker/current/config/resque.god
+  ENV=production VAGRANT_DEFAULT_PROVIDER=lxc INTERVAL=5 COUNT=1 QUEUE=rpm_worker_default,rpm_worker GROUP=rpm RESQUE_TERM_TIMEOUT=600 TERM_CHILD=1 CURRENT_PATH='/home/rosa/abf-worker/current' BACKGROUND=yes rvm ruby-2.1.1@abf-worker exec bundle exec god -c /home/rosa/abf-worker/current/config/resque.god
 else
   echo '--> worker is not dead'
   exit 1
